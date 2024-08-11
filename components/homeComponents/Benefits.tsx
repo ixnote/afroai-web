0d303b24603f5fcd6d849be59daa4530283dd94a
ismailawa aliyu <ismailawa.aliyu@gmail.com>
2024-08-11 11:05:58 +0100

    update

--- components/homeComponents/Benefits.tsx
+++ components/homeComponents/Benefits.tsx
@@ -27,79 +27,83 @@ const Benefits = () => {
         </div>
         <hr className='opacity-50 mx-8' />
       </div>
-      <div className='w-full md:w-[90%] lg:w-[90%] border rounded-xl grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 mx-auto mt-8 '>
-        <div className='text-primary-50 text-xl md:text-2xl lg:text-[45px] w-full md:w-full text-center md:text-left md:py-6 md:px-4 h-28 sm:h-auto'>
+      <div className='flex flex-col sm:flex-row w-full border rounded-xl mt-8  overflow-hidden'>
+        <div className='text-primary-50 w-full sm:w-1/3 text-xl md:text-2xl lg:text-[45px]  text-center md:text-left py-10 md:py-6 md:px-4 sm:h-auto '>
           <h2>WHY AFRO AI?</h2>
         </div>
-        <div className='flex sm:border-l-2 border-t-2 justify-between'>
-          <div className='pt-4 border-b '>
-            <Link href='#'>
-              <Image
-                src={location}
-                alt='Location Icon'
-                className='mx-4 md:mx-4'
-              />
-            </Link>
-            <div className='text-primary-50 mt-4 md:mt-8 mx-4 md:mx-4 text-center md:text-left'>
-              <h2 className='text-xl md:text-2xl font-inter'>Localization</h2>
-              <p className='py-4 md:py-8'>
-                Access innovative solutions that understand and cater to local
-                nuances
-              </p>
-              <hr className=' w-3/4 mx-auto opacity-50' />
+        <div className='flex flex-col sm:w-2/3 w-full sm:border-l-[0.5px]'>
+          <div className='flex flex-col sm:flex-row justify-between w-full sm:border-b-[0.5px]'>
+            <div className='p-6 w-full sm:w-1/2 border-t-[0.5px] sm:border-t-0'>
+              <Link href='#'>
+                <Image
+                  src={location}
+                  alt='Location Icon'
+                  className='mx-4 md:mx-4'
+                />
+              </Link>
+              <div className='text-primary-50 mt-4 md:mt-8 mx-4 md:mx-4 text-center md:text-left'>
+                <h2 className='text-xl md:text-2xl font-inter'>Localization</h2>
+                <p className='py-4 md:py-8'>
+                  Access innovative solutions that understand and cater to local
+                  nuances
+                </p>
+                <hr className=' w-full mx-auto opacity-50' />
 
-              <button className='mt-4 md:mt-8 px-4 py-1'>See Products</button>
+                <button className='mt-4 md:mt-8 px-4 py-1'>See Products</button>
+              </div>
             </div>
-          </div>
-          <div className='mx-4 md:mx-8 pt-4'>
-            <Link href='#'>
-              <Image src={box} alt='box Icon' className='' />
-            </Link>
-            <div className='text-primary-50 mt-4 md:mt-8 text-center md:text-left'>
-              <h2 className='text-xl md:text-2xl font-inter'>Affordability</h2>
-              <p className='py-4 md:py-8'>
-                Unlock the power of GPT-4 with very affordable pricing tailored
-                for African contex
-              </p>
-              <hr className=' w-3/4 mx-auto opacity-50' />
+            <div className='md:mx-8 p-6 border-t-[0.5px] sm:border-l-[0.5px] w-full sm:w-1/2'>
+              <Link href='#'>
+                <Image src={box} alt='box Icon' className='' />
+              </Link>
+              <div className='text-primary-50 mt-4 md:mt-8 text-center md:text-left'>
+                <h2 className='text-xl md:text-2xl font-inter'>
+                  Affordability
+                </h2>
+                <p className='py-4 md:py-8'>
+                  Unlock the power of GPT-4 with very affordable pricing
+                  tailored for African contex
+                </p>
+                <hr className=' w-full mx-auto opacity-50' />
 
-              <button className='mt-4 md:mt-8 px-4 py-1'>
-                {' '}
-                See Pricing Plans
-              </button>
+                <button className='mt-4 md:mt-8 px-4 py-1'>
+                  {' '}
+                  See Pricing Plans
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className='flex sm:border-l-2 border-t-2 justify-between'>
-          <div className='pt-4 border-b '>
-            <Link href='#'>
-              <Image src={head} alt='Head Icon' className='mx-4 md:mx-4' />
-            </Link>
-            <div className='text-primary-50 mt-4 md:mt-8 mx-4 md:mx-4 text-center md:text-left'>
-              <h2 className='text-xl md:text-2xl font-inter'>Community</h2>
-              <p className='py-4 md:py-8'>
-                Join the thriving ecosystem of like-minded individuals and
-                organizations driving change
-              </p>
-              <hr className=' w-3/4 mx-auto opacity-50' />
-              <button className='mt-4 md:mt-8 px-4 py-1'>
-                Visit Community
-              </button>
+          <div className='flex flex-col sm:flex-row justify-between'>
+            <div className='p-6 w-full sm:w-1/2 border-t-[0.5px] sm:border-t-0'>
+              <Link href='#'>
+                <Image src={head} alt='Head Icon' className='mx-4 md:mx-4' />
+              </Link>
+              <div className='text-primary-50 mt-4 md:mt-8 mx-4 md:mx-4 text-center md:text-left'>
+                <h2 className='text-xl md:text-2xl font-inter'>Community</h2>
+                <p className='py-4 md:py-8'>
+                  Join the thriving ecosystem of like-minded individuals and
+                  organizations driving change
+                </p>
+                <hr className=' w-full mx-auto opacity-50' />
+                <button className='mt-4 md:mt-8 px-4 py-1'>
+                  Visit Community
+                </button>
+              </div>
             </div>
-          </div>
-          <div className=' pt-4 w-full h-[51.2%] md:h-[51.3%] rounded-lg max-h-svh bg-primary-200'>
-            <Link href='#'>
-              <Image src={arrow} alt='Arrow Icon' className=' mx-4 md:mx-4' />
-            </Link>
-            <div className='text-primary-50 mx-4 md:mx-4 mt-4 md:mt-8 text-center md:text-left'>
-              <h2 className='text-xl md:text-2xl font-inter'>Need a Demo</h2>
-              <p className='py-4 md:py-8'>
-                Get firsthand experience of Afro Intelligence
-              </p>
-              <hr className=' w-3/4 mx-auto opacity-50 mt-8 md:mt-10' />
-              <button className='mt-4 md:mt-8 px-4 py-1'>
-                Try AI Assistant
-              </button>
+            <div className='p-6 w-full sm:w-1/2 rounded-lg max-h-svh bg-primary-200 border-t-[0.5px] sm:border-l-[0.5px]'>
+              <Link href='#'>
+                <Image src={arrow} alt='Arrow Icon' className=' mx-4 md:mx-4' />
+              </Link>
+              <div className='text-primary-50 mx-4 md:mx-4 mt-4 md:mt-8 text-center md:text-left'>
+                <h2 className='text-xl md:text-2xl font-inter'>Need a Demo</h2>
+                <p className='py-4 md:py-8'>
+                  Get firsthand experience of Afro Intelligence
+                </p>
+                <hr className=' w-full mx-auto opacity-50' />
+                <button className='mt-4 md:mt-8 px-4 py-1'>
+                  Try AI Assistant
+                </button>
+              </div>
             </div>
           </div>
         </div>
