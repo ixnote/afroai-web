53cfee83e414676739b025ceda251b3c3eafb3b3
Udoh Victory <victrudoh@gmail.com>
2024-10-01 16:16:03 +0100

    MERGE: Switching to dev branch

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: "standalone", // This will ensure server-side rendering
-  //output: "export",
+  output: "export",
 };
 
 export default nextConfig;
