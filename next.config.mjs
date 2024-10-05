7760968296f7717b4fc4890eae394b2b9889776a
Chukwuka Ezeaka <chukwuka.ezeaka1@outlook.com>
2024-10-05 17:40:22 +0100

    next config update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "standalone", // This will ensure server-side rendering
-  output: "export",
+  output: "standalone", // This will ensure server-side rendering
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  transpilePackages: ["@mui/x-charts"],
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    config.resolve.alias.encoding = false;
+    return config;
+  },
 };
 
 export default nextConfig;
