0ff606b8f53058a2cf4aab752a4001d614faa241
Udoh Victory <victrudoh@gmail.com>
2024-11-18 08:38:12 +0100

    UPDATE: Debugging login redirect

--- lib/oauth-options.ts
+++ lib/oauth-options.ts
@@ -93,7 +93,7 @@ export const oauthOptions: NextAuthOptions = {
     },
 
     async redirect({ url, baseUrl }) {
-      //   // Allows relative callback URLs
+      // Allows relative callback URLs
       //   if (url.startsWith('/')) return `${baseUrl}${url}`;
       //   // Allows callback URLs on the same origin
       //   else if (new URL(url).origin === baseUrl) return url;
